18ddadf9985e6c4e71f1811ebf6d8db3003b1f41
circularsprojects <admin@circularsprojects.com>
2022-07-22 11:45:14 +1000

    Initial Commit

---
+++ detnsw-autologin/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  detnsw-autologin
+//
+//  Created by Levi Rigger on 22/7/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
