6821811ac24deb90a78692a5c6704405792958b7
circularsprojects <admin@circularsprojects.com>
2022-07-25 13:04:33 +1000

    working with keychain services and quitting the app when you press the close button

--- detnsw-autologin/ViewController.swift
+++ detnsw-autologin/ViewController.swift
@@ -7,7 +7,7 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSWindowDelegate {
     let defaults = UserDefaults.standard
     @IBOutlet var username: NSTextField!
     @IBOutlet var password: NSSecureTextField!
@@ -17,6 +17,14 @@ class ViewController: NSViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear() {
+        self.view.window?.delegate = self
+    }
+    
+    func windowShouldClose(_ sender: NSWindow) -> Bool {
+        exit(1)
+    }
 
     override var representedObject: Any? {
         didSet {
@@ -25,12 +33,16 @@ class ViewController: NSViewController {
     }
 
     @IBAction func loginButton(_ sender: NSButton) {
-        let username = defaults.string(forKey: "username")
-        password.stringValue = username ?? "null"
+        let username = defaults.string(forKey: "username")!
+        //password.stringValue = username ?? "null"
+        let password = String(decoding: kread(service: "detnsw-autologin", account: username)!, as: UTF8.self)
+        print(password)
     }
     
     @IBAction func saveButton(_ sender: NSButton) {
         defaults.set(username.stringValue, forKey: "username")
+        let utf8p = password.stringValue.utf8
+        ksave(Data(utf8p), service: "detnsw-autologin", account: username.stringValue)
     }
     
 }
