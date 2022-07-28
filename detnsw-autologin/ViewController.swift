513569fde68a58638086e9ecde7f1bc156418dc5
circularsprojects <admin@circularsprojects.com>
2022-07-28 12:21:51 +1000

    idk new stuff

--- detnsw-autologin/ViewController.swift
+++ detnsw-autologin/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import Cocoa
+import ServiceManagement
 
 class ViewController: NSViewController, NSWindowDelegate {
     let defaults = UserDefaults.standard
@@ -33,17 +34,44 @@ class ViewController: NSViewController, NSWindowDelegate {
     }
 
     @IBAction func loginButton(_ sender: NSButton) {
-        let username = defaults.string(forKey: "username")!
+        let usernamestored = defaults.string(forKey: "username")!
         //password.stringValue = username ?? "null"
-        let password = String(decoding: kread(service: "detnsw-autologin", account: username)!, as: UTF8.self)
-        print(password)
+        let passwordstored = String(decoding: kread(service: "detnsw-autologin", account: usernamestored)!, as: UTF8.self)
+        print(usernamestored)
+        print(passwordstored)
+        
+        let url = URL(string:"https://edgeportal.forti.net.det.nsw.edu.au/portal/selfservice/IatE_CP/")
+        //let url = URL(string: "https://jsonplaceholder.typicode.com/todos")
+        guard let requestUrl = url else { fatalError() }
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+        let poststring = "csrfmiddlewaretoken=&username=\(username.stringValue)&password=\(password.stringValue)"
+        request.httpBody = poststring.data(using: String.Encoding.utf8)
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+            if let data = data, let _ = String(data: data, encoding: .utf8) {
+                //print("Response data string:\n \(dataString)")
+                //print(response.statusCode)
+            }
+        }
+        task.resume()
     }
     
     @IBAction func saveButton(_ sender: NSButton) {
-        defaults.set(username.stringValue, forKey: "username")
-        let utf8p = password.stringValue.utf8
-        ksave(Data(utf8p), service: "detnsw-autologin", account: username.stringValue)
+        let usernamestored = defaults.string(forKey: "username")
+        if (username.stringValue == usernamestored) {
+            let utf8p = password.stringValue.utf8
+            ksave(Data(utf8p), service: "detnsw-autologin", account: username.stringValue)
+        } else {
+            kdelete(service: "detnsw-autologin", account: usernamestored!)
+            defaults.set(username.stringValue, forKey: "username")
+            let utf8p = password.stringValue.utf8
+            ksave(Data(utf8p), service: "detnsw-autologin", account: username.stringValue)
+        }
     }
-    
 }
 
