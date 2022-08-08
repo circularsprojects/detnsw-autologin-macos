6bafd8d962e262e30e189a6c9f527a2ca766cf1b
circularsprojects <admin@circularsprojects.com>
2022-08-09 09:26:39 +1000

    trying a new approach

--- detnsw-autologin-helper/HelperAppDelegate.swift
+++ detnsw-autologin-helper/HelperAppDelegate.swift
@@ -11,32 +11,33 @@ import Network
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
     let defaults = UserDefaults.init(suiteName: "QX9LC82293.com.circularsprojects.detnsw-autologin-group")
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        let usernamestored = defaults!.string(forKey: "username")!
-        let passwordstored = String(decoding: kread(service: "detnsw-autologin", account: usernamestored)!, as: UTF8.self)
-        
-        let url = URL(string:"https://edgeportal.forti.net.det.nsw.edu.au/portal/selfservice/IatE_CP/")
-        guard let requestUrl = url else { fatalError() }
-        var request = URLRequest(url: requestUrl)
-        request.httpMethod = "POST"
-        let poststring = "csrfmiddlewaretoken=&username=\(usernamestored)&password=\(passwordstored)"
-        request.httpBody = poststring.data(using: String.Encoding.utf8)
-        
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let error = error {
-                print("Error took place \(error)")
-                return
-            }
-            if let data = data, let _ = String(data: data, encoding: .utf8) {
-                //print("Response data string:\n \(dataString)")
-                //print(response.statusCode)
-            }
-        }
-        
         let monitor = NWPathMonitor()
-        monitor.pathUpdateHandler = { path in
+        monitor.pathUpdateHandler = { [self] path in
             if path.status == .satisfied {
+                let usernamestored = defaults!.string(forKey: "username")!
+                let passwordstored = String(decoding: kread(service: "detnsw-autologin", account: usernamestored)!, as: UTF8.self)
+                
+                let url = URL(string:"https://edgeportal.forti.net.det.nsw.edu.au/portal/selfservice/IatE_CP/")
+                guard let requestUrl = url else { fatalError() }
+                var request = URLRequest(url: requestUrl)
+                request.httpMethod = "POST"
+                let poststring = "csrfmiddlewaretoken=&username=\(usernamestored)&password=\(passwordstored)"
+                request.httpBody = poststring.data(using: String.Encoding.utf8)
+                
+                let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                    if let error = error {
+                        print("Error took place \(error)")
+                        return
+                    }
+                    if let data = data, let _ = String(data: data, encoding: .utf8) {
+                        //print("Response data string:\n \(dataString)")
+                        //print(response.statusCode)
+                    }
+                }
                 task.resume()
+                print("Internet connected")
             }
         }
         let queue = DispatchQueue(label: "Monitor")
